f71b43c63bda3dc3b60fe49bb0f8d435d745b2bb
Yizhou Zhu <mkingzhu@vip.qq.com>
2015-08-12 17:05:49 +0800

    add WebService

--- src/main/java/com/mkingzhu/wechat/center/server/webservice/facade/WechatTokenService.java
+++ src/main/java/com/mkingzhu/wechat/center/server/webservice/facade/WechatTokenService.java
@@ -1,8 +1,11 @@
 package com.mkingzhu.wechat.center.server.webservice.facade;
 
+import javax.jws.WebService;
+
 import me.chanjar.weixin.common.bean.WxJsapiSignature;
 import me.chanjar.weixin.common.exception.WxErrorException;
 
+@WebService
 public interface WechatTokenService {
     public String getAccessToken() throws WxErrorException;
 
